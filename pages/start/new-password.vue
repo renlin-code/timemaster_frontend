e70c2139d2f8111b5076e633a64a5109fca4b647
renlin-code <renelj1997@gmail.com>
2023-01-01 15:14:32 +0500

    New password page ready and well working

--- pages/start/new-password.vue
+++ pages/start/new-password.vue
@@ -9,22 +9,36 @@
     </template>
 
     <template #inner>
+      <FormPreloader
+        v-if="pending"
+      />
+
       <div class="new-password__form">
         <div class="new-password__form-top">
           <div class="new-password__form-inputs">
             <StartInput
+              required
               type="password"
               placeholder="New password"
+              v-model="password1"
+              :trigger="validateTrigger"
+              @validate="validateResponse"
             />
             <StartInput
+              required
               type="password"
               placeholder="Repeat password"
+              v-model="password2"
+              :trigger="validateTrigger"
+              @validate="validateResponse"
+              :injectedErrorMessage="errorMessage"
             />
           </div>
         </div>
         <div class="new-password__form-bottom">
           <MainButton
             type="1"
+            @click.native="validate"
           >
             Ok
           </MainButton>
@@ -38,9 +52,69 @@
 import MainButton from '~/components/buttons/MainButton.vue';
 import StartInput from '~/components/inputs/StartInput.vue';
 import StartPage from '~/components/layout/StartPage.vue';
+import FormPreloader from '~/components/preloaders/FormPreloader.vue';
 
 export default {
-    components: { StartPage, StartInput, MainButton }
+    components: { StartPage, StartInput, MainButton, FormPreloader },
+    data: () => ({
+    validateTrigger: false,
+    pending: false,
+    validateStack: [],
+    errorMessage: "",
+
+    password1: "",
+    password2: "",
+
+    dataForPost: {
+      token: "",
+      newPassword: ""
+    }
+  }),
+  methods: {
+    validate(){
+      this.errorMessage = "";
+
+      this.validateStack = [];
+      this.validateTrigger = !this.validateTrigger;
+    },
+    validateResponse(error){
+      this.validateStack.push(error)
+
+      const count = this.$el.querySelectorAll('*[required]').length
+      if(this.validateStack.length === count && this.validateStack.indexOf(true) === -1 && !this.pending){
+        if (this.password1 !== this.password2) {
+          this.errorMessage = "Passwords do not match"
+        } else {
+          this.dataForPost.newPassword = this.password1;
+          this.submit()
+        }
+      }
+    },
+
+    async submit() {
+      try {
+        this.pending = true;
+        const res = await this.$axios.$post("/auth/change-password", this.dataForPost);
+        this.pending = false;
+
+        const authToken = res.token;
+        localStorage.setItem("authToken", authToken);
+        localStorage.setItem("savedDevice", true);
+
+        this.$router.push("/");
+      } catch (error) {
+        this.pending = false;
+      }
+    }
+  },
+  created() {
+    const queryToken = this.$route.query.token;
+    if (!queryToken) {
+      this.$nuxt.error("404");
+    } else {
+      this.dataForPost.token = queryToken;
+    }
+  }
 }
 </script>
 
--- pages/start/recovery.vue
+++ pages/start/recovery.vue
@@ -9,16 +9,39 @@
     </template>
 
     <template #inner>
+      <StartModal
+        v-if=showModal
+        @close="showModal = false"
+      >
+        <template #main-text>
+          Please check out your email, we have sent you a password recovery email
+        </template>
+        <template #button-text>
+          Ok
+        </template>
+      </StartModal>
+
+      <FormPreloader
+        v-if="pending"
+      />
+
+
       <div class="recovery__form">
         <div class="recovery__form-top">
           <StartInput
+            required
             type="email"
             placeholder="Email"
+            v-model="dataForPost.email"
+            :trigger="validateTrigger"
+            @validate="validateResponse"
+            :injectedErrorMessage="errorMessage"
           />
         </div>
         <div class="recovery__form-bottom">
           <MainButton
             type="1"
+            @click.native="validate"
           >
             Send me email
           </MainButton>
@@ -32,9 +55,52 @@
 import MainButton from '~/components/buttons/MainButton.vue';
 import StartInput from '~/components/inputs/StartInput.vue';
 import StartPage from '~/components/layout/StartPage.vue';
+import StartModal from '~/components/modals/StartModal.vue';
+import FormPreloader from '~/components/preloaders/FormPreloader.vue';
 
 export default {
-    components: { StartPage, StartInput, MainButton }
+    components: { StartPage, StartInput, MainButton, StartModal, FormPreloader },
+    data: () => ({
+    validateTrigger: false,
+    pending: false,
+    showModal: false,
+    validateStack: [],
+    errorMessage: "",
+
+    dataForPost: {
+      email: ""
+    }
+  }),
+  methods: {
+    validate(){
+      this.errorMessage = "";
+      this.validateStack = [];
+      this.validateTrigger = !this.validateTrigger
+    },
+    validateResponse(error){
+      this.validateStack.push(error)
+
+      const count = this.$el.querySelectorAll('*[required]').length
+      if(this.validateStack.length === count && this.validateStack.indexOf(true) === -1 && !this.pending){
+        this.submit()
+      }
+    },
+
+    async submit() {
+      try {
+        this.pending = true;
+        const res = await this.$axios.$post("/auth/recovery", this.dataForPost);
+        this.pending = false;
+        this.showModal = true;
+
+      } catch (error) {
+        this.pending = false;
+        if(error.response.data.statusCode == 404) {
+          this.errorMessage = "Wrong email";
+        }
+      }
+    }
+  }
 }
 </script>
 
